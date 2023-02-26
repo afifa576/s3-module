962d6d23612d07498c1fc635cf933d8c5660ec15
afifa576 <124342341+afifa576@users.noreply.github.com>
2023-02-26 13:44:59 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 resource "aws_s3_bucket" "sample" {
     bucket = var.bucket_name
+    aws_s3_bucket_versioning=var.versioning
 
 
-    versioning {
-
-        enabled = var.versioning
-    }
+    
 }
   
